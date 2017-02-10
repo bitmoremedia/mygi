fc08ad4210da9a530256641b090fd8925a78d33c
Rusta <rustauk@gmail.com>
2017-02-10 14:37:53 +0000

    use reactGA lib

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,15 @@
+import ReactGA from 'react-ga';
+
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-72779821-3');
+}
+
+exports.onRouteUpdate = (state, page, pages) => { // eslint-disable-line
+  if (ReactGA) {
+    ReactGA.pageview(state.pathname);
+  }
+};
+
 exports.shouldUpdateScroll = function(prevRouterProps, nextRouterProps) {
   if (prevRouterProps){
     // do not scroll if the user is moving between routes on the 'glycemic-index' page
