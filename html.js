37ab1662dc9494ba01ef24ac9c3f18db05fabfb8
Rusta <rustauk@gmail.com>
2017-02-10 11:29:23 +0000

    add favicon, app icons and open graph meta tags; added an _public directory to house the static public assets (to do: make refreshing this part of the build step)

--- html.js
+++ html.js
@@ -3,6 +3,8 @@ import Helmet from 'react-helmet';
 import { prefixLink } from 'gatsby-helpers';
 import { config } from 'config';
 
+import packageJson from './package.json';
+
 const BUILD_TIME = new Date().getTime();
 
 const HTML = (props) => {
@@ -29,10 +31,28 @@ const HTML = (props) => {
   `;
 
   return (
-    <html lang="en">
+    <html lang="en" data-version={packageJson.version}>
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta property="og:title" content="MyGi - Glycemic Index" />
+        <meta property="og:description" content="MyGi App companion website and home of the officially unofficial Glyemic Index Food List" />
+        <meta property="og:image" content="https://mygi.io/mygi-app-icon.png" />
+        <meta property="og:url" content="https://mygi.io" />
+        <meta property="og:site_name" content="MyGi" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192"  href="/icons/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
