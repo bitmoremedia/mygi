671d7f03f045d1cfd80987c233708eb3ca123071
Rusta <rustauk@gmail.com>
2017-02-15 11:31:37 +0000

    make modal content scrollable; stop background from scrolling when modal is open;

--- dataTool/src/components/common/Modal/index.js
+++ dataTool/src/components/common/Modal/index.js
@@ -4,6 +4,8 @@ import Rodal from 'rodal';
 // note: this component has a dependency on global styles
 // import 'rodal/lib/rodal.css';
 
+import { ModalContainer } from './styles';
+
 const modalProps = {
   visible: React.PropTypes.bool.isRequired,
   onClose: React.PropTypes.func.isRequired,
@@ -14,9 +16,19 @@ const modalProps = {
 }
 
 const Modal = ({ children, visible, onClose, width, height, measure }) => {
+
+  // stop body scrolling when modal is open
+  if (visible){
+    document.body.style.overflowY = "hidden";
+  } else {
+    document.body.style.overflowY = "scroll";
+  }
+
   return (
       <Rodal visible={visible} onClose={onClose} width={width} height={height} measure={measure}>
-        {children}
+        <ModalContainer>
+          {children}
+        </ModalContainer>
       </Rodal>
   );
 }
---
+++ dataTool/src/components/common/Modal/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  overflow: scroll;
+  height: 100%;
+`;
