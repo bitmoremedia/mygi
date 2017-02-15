1cdacd4eb3a2ca324242e8c2300f78ee86285e72
Rusta <rustauk@gmail.com>
2017-02-15 23:57:44 +0000

    cors lib to the rescue

--- dataTool/server/app.js
+++ dataTool/server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -6,28 +7,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const api = require('./api');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
-app.all('/', function(req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//  next();
-
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  // Set custom headers for CORS
-  res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
-
-  if (req.method === "OPTIONS") {
-      return res.status(200).end();
-  }
-
-  return next();
-
-});
 
 app.post('/api/*', (req, res) => {
   const { url, method, params, body } = req;
