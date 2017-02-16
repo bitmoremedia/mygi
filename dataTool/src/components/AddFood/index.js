e3a5e70ca00b79d73671c9d792f336a49cec14bf
Rusta <rustauk@gmail.com>
2017-02-16 11:26:17 +0000

    bare bones form control for Add Food component

--- dataTool/src/components/AddFood/index.js
+++ dataTool/src/components/AddFood/index.js
@@ -4,16 +4,35 @@ export class AddFood extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      foodName: "",
+      name: "",
       gi: "",
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('submit form', this.state);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
   }
 
   render() {
     return (
-      <div>
-        <h1>Add Food</h1>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type='text' name='name' value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <label>
+          GI:
+          <input type='number' name='gi' value={this.state.gi} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Add" />
+      </form>
     );
   }
 
