0a9a10a1cacb0e011fad3f8674c91ec22e5c29b1
Rusta <rustauk@gmail.com>
2017-02-16 22:17:00 +0000

    close Modal Component if user presses the escape key while the Modal is open

--- dataTool/src/components/common/Modal/index.js
+++ dataTool/src/components/common/Modal/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Rodal from 'rodal';
 
 // note: this component has a dependency on global styles
@@ -6,24 +6,48 @@ import Rodal from 'rodal';
 
 import { ModalContainer } from './styles';
 
-const Modal = ({ children, visible, onClose, width, height, measure }) => {
+class Modal extends Component {
 
-  // stop the background from scrolling (if scroll bars are on the page)
-  if (visible){
-    if ( window.innerWidth > document.documentElement.clientWidth ){
-      document.body.classList.add('modal-is-open');
-    }
-  } else {
-    document.body.classList.remove('modal-is-open');
+  constructor(props, context) {
+    super(props, context);
+    this.closeModalOnEscapePress = this.closeModalOnEscapePress.bind(this);
   }
 
-  return (
-      <Rodal visible={visible} onClose={onClose} width={width} height={height} measure={measure}>
-        <ModalContainer>
-          {children}
-        </ModalContainer>
-      </Rodal>
-  );
+  componentWillUnmount(){
+    // remove listener for closing modal on press of escape key
+    document.removeEventListener("keydown", this.closeModalOnEscapePress);
+  }
+
+  closeModalOnEscapePress(event) {
+    if (event.keyCode === 27) {
+      this.props.onClose(event);
+    }
+  }
+
+  render() {
+    const { closeModalOnEscapePress } = this;
+    const { children, visible, onClose, width, height, measure } = this.props;
+    // stop the background from scrolling (if scroll bars are on the page)
+    if (visible){
+      // add listener for closing modal on press of escape key
+      document.addEventListener("keydown", closeModalOnEscapePress);
+      if ( window.innerWidth > document.documentElement.clientWidth ){
+        document.body.classList.add('modal-is-open');
+      }
+    } else {
+      // remove listener for closing modal on press of escape key
+      document.removeEventListener("keydown", closeModalOnEscapePress);
+      document.body.classList.remove('modal-is-open');
+    }
+
+    return (
+        <Rodal visible={visible} onClose={onClose} width={width} height={height} measure={measure}>
+          <ModalContainer>
+            {children}
+          </ModalContainer>
+        </Rodal>
+    );
+  }
 }
 
 Modal.propTypes =  {
