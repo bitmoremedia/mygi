9de41c9e5b39195ed0570eb39df4f8c0da6e7d4c
Rusta <rustauk@gmail.com>
2017-02-16 10:05:10 +0000

    bulk import action types

--- dataTool/src/redux/actions/index.js
+++ dataTool/src/redux/actions/index.js
@@ -1,17 +1,5 @@
 import * as api from '../../api';
-import {
-  GET_FOODLIST_REQUEST,
-  GET_FOODLIST_SUCCESS,
-  GET_FOODLIST_ERROR,
-  RECEIVE_FOODLIST,
-  GET_DATASOURCES_REQUEST,
-  GET_DATASOURCES_SUCCESS,
-  GET_DATASOURCES_ERROR,
-  RECEIVE_DATASOURCES,
-  POST_ASSOCIATE_DATASOURCE_REQUEST,
-  POST_ASSOCIATE_DATASOURCE_SUCCESS,
-  POST_ASSOCIATE_DATASOURCE_ERROR,
-} from './types';
+import * as types from './types';
 
 // **********************
 // FOODLIST
@@ -19,20 +7,20 @@ import {
 
 export const receiveFoodlist = (data) => {
   return {
-    type: RECEIVE_FOODLIST,
+    type: types.RECEIVE_FOODLIST,
     payload: data
   }
 };
 
 export const getFoodlist = () => async (dispatch) => {
   try {
-    dispatch({ type: GET_FOODLIST_REQUEST });
+    dispatch({ type: types.GET_FOODLIST_REQUEST });
     let data = await api.getFoodList();
-    dispatch({ type: GET_FOODLIST_SUCCESS });
+    dispatch({ type: types.GET_FOODLIST_SUCCESS });
     dispatch(receiveFoodlist(data))
   } catch(error) {
     console.log('error', error);
-    dispatch({ type: GET_FOODLIST_ERROR, payload: error });
+    dispatch({ type: types.GET_FOODLIST_ERROR, payload: error });
   }
 }
 
@@ -42,20 +30,20 @@ export const getFoodlist = () => async (dispatch) => {
 
 export const receiveDataSources = (data) => {
   return {
-    type: RECEIVE_DATASOURCES,
+    type: types.RECEIVE_DATASOURCES,
     payload: data
   }
 };
 
 export const getDataSources = () => async (dispatch) => {
   try {
-    dispatch({ type: GET_DATASOURCES_REQUEST });
+    dispatch({ type: types.GET_DATASOURCES_REQUEST });
     let data = await api.getDataSources();
-    dispatch({ type: GET_DATASOURCES_SUCCESS });
+    dispatch({ type: types.GET_DATASOURCES_SUCCESS });
     dispatch(receiveDataSources(data))
   } catch(error) {
     console.log('error', error);
-    dispatch({ type: GET_DATASOURCES_ERROR, payload: error });
+    dispatch({ type: types.GET_DATASOURCES_ERROR, payload: error });
   }
 }
 
@@ -65,18 +53,18 @@ export const getDataSources = () => async (dispatch) => {
 
 export const associateDataSource = (data) => async (dispatch) => {
   try {
-    dispatch({ type: POST_ASSOCIATE_DATASOURCE_REQUEST });
+    dispatch({ type: types.POST_ASSOCIATE_DATASOURCE_REQUEST });
     let response = await api.postAssociateDataSource(data);
     if ( response.status === 'success' ){
-      dispatch({ type: POST_ASSOCIATE_DATASOURCE_SUCCESS });
+      dispatch({ type: types.POST_ASSOCIATE_DATASOURCE_SUCCESS });
       // update was a success so we refetch the food list
       dispatch(getFoodlist());
     } else {
       console.log('error', response.error);
-      dispatch({ type: POST_ASSOCIATE_DATASOURCE_ERROR, payload: response.error });
+      dispatch({ type: types.POST_ASSOCIATE_DATASOURCE_ERROR, payload: response.error });
     }
   } catch(error) {
     console.log('error', error);
-    dispatch({ type: POST_ASSOCIATE_DATASOURCE_ERROR, payload: error });
+    dispatch({ type: types.POST_ASSOCIATE_DATASOURCE_ERROR, payload: error });
   }
 };
