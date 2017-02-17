0a284878851bc4bfa73ee3e8c11c85aca8305fd1
Rusta <rustauk@gmail.com>
2017-02-17 08:31:59 +0000

    update sanity check App test to work with Redux

--- dataTool/src/components/App.test.js
+++ dataTool/src/components/App.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from '../redux/reducers';
 import { App } from './App';
+
+// mock APIs
+function getFoodlist(){
+  return [];
+}
+function getDataSources(){
+  return [];
+}
+const store = createStore(reducers);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App getFoodlist={getFoodlist} getDataSources={getDataSources} /></Provider>, div);
 });
