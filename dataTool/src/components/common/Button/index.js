ed4fe833abc8b122b65cb09752d529548d1db915
Rusta <rustauk@gmail.com>
2017-02-23 11:01:17 +0000

    rename styles file to styled-components

--- dataTool/src/components/common/Button/index.js
+++ dataTool/src/components/common/Button/index.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 
-import { Button, DangerButton } from './styles'
+import { Button, DangerButton } from './styled-components'
 
 const ButtonComponent = ({ children, onClick, danger }) => {
   if (danger) {
