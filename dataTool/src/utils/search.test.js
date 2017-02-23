4545f318562bc7d8bb4dd7473bb1702ef0db8bf8
Rusta <rustauk@gmail.com>
2017-02-23 16:01:21 +0000

    lint proof search utils tests

--- dataTool/src/utils/search.test.js
+++ dataTool/src/utils/search.test.js
@@ -1,116 +1,115 @@
-import { search } from './search';
+import { search } from './search'
 
 const data = [
-  { "name": "Paul" },
-  { "name": "Barry"},
-  { "name": "Larry"},
-  { "name": "Beth"},
-  { "name": "Bill Oddy"},
-  { "name": "Steve Coogan"},
-  { "name": "russthedude"},
-  { "name": "Dwayne/Dibbly"},
-  { "name": "Dwayne Dibbly"},
-  { "name": "Dwayne-Dibbly"},
-  { "name": "Darren Dibbly"},
-];
+  { name: 'Paul' },
+  { name: 'Barry' },
+  { name: 'Larry' },
+  { name: 'Beth' },
+  { name: 'Bill Oddy' },
+  { name: 'Steve Coogan' },
+  { name: 'russthedude' },
+  { name: 'Dwayne/Dibbly' },
+  { name: 'Dwayne Dibbly' },
+  { name: 'Dwayne-Dibbly' },
+  { name: 'Darren Dibbly' },
+]
 
-describe('Utils: search', ()=>{
-  const searchField = 'name';
-  const searchList = data;
-  let searchResult, searchText, expectedResult;
+describe('Utils: search', () => {
+  const searchField = 'name'
+  let searchList = data
+  let searchResult, searchText, expectedResult
 
   it('basic search returns some data', () => {
-    searchText = 'Paul';
-    searchResult = search({searchText, searchList, searchField});
-    expect(searchResult).toBeDefined();
-  });
+    searchText = 'Paul'
+    searchResult = search({ searchText, searchList, searchField })
+    expect(searchResult).toBeDefined()
+  })
 
   it('returns case insensitive matches', () => {
-    searchText = 'beth';
-    searchResult = search({searchText, searchList, searchField});
+    searchText = 'beth'
+    searchResult = search({ searchText, searchList, searchField })
     expectedResult = [
-      { "name": "Beth"},
-    ];
-    expect(searchResult).toEqual(expectedResult);
-  });
+      { name: 'Beth' },
+    ]
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('returns partial matching text items', () => {
-    searchText = 'arry';
-    searchResult = search({searchText, searchList, searchField});
+    searchText = 'arry'
+    searchResult = search({ searchText, searchList, searchField })
     expectedResult = [
-      { "name": "Barry"},
-      { "name": "Larry"},
-    ];
-    expect(searchResult).toEqual(expectedResult);
-  });
+      { name: 'Barry' },
+      { name: 'Larry' },
+    ]
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('returns partial word matches - space delimited', () => {
-    searchText = 'Ben Oddy';
-    searchResult = search({searchText, searchList, searchField});
+    searchText = 'Ben Oddy'
+    searchResult = search({ searchText, searchList, searchField })
     expectedResult = [
-      { "name": "Bill Oddy"},
-    ];
-    expect(searchResult).toEqual(expectedResult);
-  });
+      { name: 'Bill Oddy' },
+    ]
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('returns partial word matches - slash delimited', () => {
-    searchText = 'Ben/Oddy';
-    searchResult = search({searchText, searchList, searchField});
+    searchText = 'Ben/Oddy'
+    searchResult = search({ searchText, searchList, searchField })
     expectedResult = [
-      { "name": "Bill Oddy"},
-    ];
-    expect(searchResult).toEqual(expectedResult);
-  });
+      { name: 'Bill Oddy' },
+    ]
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('returns partial word matches - hyphen delimited', () => {
-    searchText = 'Dwayne Dibbly';
-    searchResult = search({searchText, searchList, searchField});
+    searchText = 'Dwayne Dibbly'
+    searchResult = search({ searchText, searchList, searchField })
     expectedResult = [
-      { "name": "Dwayne/Dibbly"},
-      { "name": "Dwayne Dibbly"},
-      { "name": "Dwayne-Dibbly"},
-      { "name": "Darren Dibbly"},
-    ];
-    expect(searchResult).toEqual(expectedResult);
-  });
+      { name: 'Dwayne/Dibbly' },
+      { name: 'Dwayne Dibbly' },
+      { name: 'Dwayne-Dibbly' },
+      { name: 'Darren Dibbly' },
+    ]
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('returns white space in target matches', () => {
-    searchText = 'stevecoogan';
-    searchResult = search({searchText, searchList, searchField});
+    searchText = 'stevecoogan'
+    searchResult = search({ searchText, searchList, searchField })
     expectedResult = [
-      { "name": "Steve Coogan"},
-    ];
-    expect(searchResult).toEqual(expectedResult);
-  });
+      { name: 'Steve Coogan' },
+    ]
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('returns white space in source matches', () => {
-    searchText = 'Russ The Dude';
-    searchResult = search({searchText, searchList, searchField});
+    searchText = 'Russ The Dude'
+    searchResult = search({ searchText, searchList, searchField })
     expectedResult = [
-      { "name": "russthedude"},
-    ];
-    expect(searchResult).toEqual(expectedResult);
-  });
+      { name: 'russthedude' },
+    ]
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('specific use case A', () => {
-    const searchText = '100% Whole Grain® bread (Natural Ovens)';
-    const searchList = [
-      { "name" : "Whole wheat/whole meal bread" },
-      { "name" : "Wholemeal bread" }
-    ];
-    expectedResult = searchList;
-    const searchResult = search({searchText, searchList, searchField});
-    expect(searchResult).toEqual(expectedResult);
-  });
+    searchText = '100% Whole Grain® bread (Natural Ovens)'
+    searchList = [
+      { name: 'Whole wheat/whole meal bread' },
+      { name: 'Wholemeal bread' },
+    ]
+    expectedResult = searchList
+    searchResult = search({ searchText, searchList, searchField })
+    expect(searchResult).toEqual(expectedResult)
+  })
 
   it('specific use case B', () => {
-    const searchText = 'Pear, raw';
-    const searchList = [
-      { "name" : "Pears" },
-    ];
-    expectedResult = searchList;
-    const searchResult = search({searchText, searchList, searchField});
-    expect(searchResult).toEqual(expectedResult);
-  });
-
-});
+    searchText = 'Pear, raw'
+    searchList = [
+      { name: 'Pears' },
+    ]
+    expectedResult = searchList
+    searchResult = search({ searchText, searchList, searchField })
+    expect(searchResult).toEqual(expectedResult)
+  })
+})
