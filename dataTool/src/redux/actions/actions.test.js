06adcddd34565ffd3383d5eae160d6e3ce5bead9
Rusta <rustauk@gmail.com>
2017-02-24 09:27:35 +0000

    attempt at async action creator test (no mocking of API data yet)

--- dataTool/src/redux/actions/actions.test.js
+++ dataTool/src/redux/actions/actions.test.js
@@ -1,16 +1,41 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
 import * as types from './types'
 import * as actions from './index'
 
-it('receiveFoodlist should create RECEIVE_FOODLIST action', () => {
-  expect(actions.receiveFoodlist([])).toEqual({
-    type: types.RECEIVE_FOODLIST,
-    payload: [],
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+describe('sync actions', () => {
+  it('receiveFoodlist should create RECEIVE_FOODLIST action', () => {
+    expect(actions.receiveFoodlist([])).toEqual({
+      type: types.RECEIVE_FOODLIST,
+      payload: [],
+    })
+  })
+
+  it('receiveDataSources should create RECEIVE_DATASOURCES action', () => {
+    expect(actions.receiveDataSources([])).toEqual({
+      type: types.RECEIVE_DATASOURCES,
+      payload: [],
+    })
   })
 })
 
-it('receiveDataSources should create RECEIVE_DATASOURCES action', () => {
-  expect(actions.receiveDataSources([])).toEqual({
-    type: types.RECEIVE_DATASOURCES,
-    payload: [],
+
+describe('async actions', () => {
+  it('creates FETCH_TODOS_SUCCESS when fetching todos has been done', () => {
+
+    const expectedActions = [
+      { type: types.FETCH_TODOS_REQUEST },
+      { type: types.FETCH_TODOS_SUCCESS, body: { todos: ['do something'] } },
+    ]
+    const store = mockStore({})
+
+    return store.dispatch(actions.getFoodlist())
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions)
+      })
   })
 })
